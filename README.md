bec2904a74ce43a840bbcecba42c5697dabbf6fc
saari5 <saari5@tuni.fi>
2019-10-03 10:12:24 +0300

    tietokannan tyhjennys kommentoitu pois

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AkunTaskukirjat2019
 AkunTaskukirjat2019
+
+ * Kun sovellus on toimiva, niin tietokannan tyhjennys ja testidatan syöttö alussa voidaan kommentoida pois TaskukirjaRoomDatabase.java luokasta
+
+```java
+  /*
+    mDao.deleteAll();
+    Taskukirja taskukirja=new Taskukirja(1, "Mikki kiipelissä");
+    mDao.insert(taskukirja);
+    taskukirja=new Taskukirja(2, "Aku Ankka ja Karhukopla");
+    mDao.insert(taskukirja);
+  */
+```
--- taskarit2019/app/src/main/java/fi/tuni/miksa/taskari2019/TaskukirjaRoomDatabase.java
+++ taskarit2019/app/src/main/java/fi/tuni/miksa/taskari2019/TaskukirjaRoomDatabase.java
@@ -57,12 +57,16 @@ public abstract class TaskukirjaRoomDatabase extends RoomDatabase {
 
             Log.d("softa", "Ajettiin taas");
 
-            mDao.deleteAll();
+            //Kun sovellus on saatu toimimaan, niin tietokannan tyhjennys ja testidatan syöttö voidaan kommentoida pois
+
+            /*mDao.deleteAll();
             Taskukirja taskukirja=new Taskukirja(1, "Mikki kiipelissä");
             mDao.insert(taskukirja);
             taskukirja=new Taskukirja(2, "Aku Ankka ja Karhukopla");
             mDao.insert(taskukirja);
+            */
             return null;
         }
     }
+
 }
